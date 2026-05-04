b013d0238c3d9e07963fd072d00b4b4a3db0736b
FelipeCalaca <felipecalacasi@outlook.com>
2026-05-04 20:52:57 -0300

    comito

--- spots/app/page.tsx
+++ spots/app/page.tsx
@@ -1,5 +1,5 @@
 // Importamos o componente que acabamos de criar
-import LocalCard from './components/LocalCard';
+import LocalCard from "../components/LocalCard";
 
 export default function HomePage() {
   return (
