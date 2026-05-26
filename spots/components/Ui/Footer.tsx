6440099b73f82ef535817492cb68e9c39479607f
Álefe <alefemiguel54@gmail.com>
2026-05-26 20:24:57 -0300

    att

---
+++ spots/components/Ui/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <footer>
+            <h3>Entre em contato conosco:</h3>
+            <h3>Contato: 629 9999-9999</h3>
+        </footer>
+
+    );
+
+}
